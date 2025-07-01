4cacc61bbdb11d1898ac580fe249e9904d18953f
Babbie <11524583+Babbie@users.noreply.github.com>
2025-07-01 15:51:24 +0200

    Add REI support

---
+++ src/main/java/net/davio/create_aquatic_ambitions/compat/rei/category/FanChannelingCategory.java
@@ -0,0 +1,30 @@
+package net.davio.create_aquatic_ambitions.compat.rei.category;
+
+import com.simibubi.create.compat.rei.category.ProcessingViaFanCategory;
+import com.simibubi.create.compat.rei.category.animations.AnimatedKinetics;
+
+import com.simibubi.create.foundation.gui.AllGuiTextures;
+import com.simibubi.create.foundation.gui.element.GuiGameElement;
+import net.davio.create_aquatic_ambitions.compat.rei.category.animations.AnimatedConduit;
+import net.davio.create_aquatic_ambitions.kinetics.fan.processing.ChannelingRecipe;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.world.level.block.Blocks;
+
+public class FanChannelingCategory extends ProcessingViaFanCategory.MultiOutput<ChannelingRecipe> {
+
+	private final AnimatedConduit conduit = new AnimatedConduit();
+
+	public FanChannelingCategory(Info<ChannelingRecipe> info) {
+		super(info);
+	}
+
+	@Override
+	protected AllGuiTextures getBlockShadow() {
+		return AllGuiTextures.JEI_LIGHT;
+	}
+
+	@Override
+	protected void renderAttachedBlock(GuiGraphics graphics) {
+		conduit.draw(graphics, 0, 0);
+	}
+}
